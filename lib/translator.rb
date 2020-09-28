7746489123d50ff4f2547edd2dfc3873d19d53d2
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-09-28 01:19:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,7 +10,7 @@ def load_library(emoticon_file)
 
   emoticons.each do |meaning, value|
     english = value[0]
-    japanese = value [1]
+    japanese = value[1]
     emoticon_lib[:get_meaning][japanese] = meaning
     emoticon_lib[:get_emoticon][english] = japanese
   end
