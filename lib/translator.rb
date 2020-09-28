949f15e7784298f7a75dffaa0db04d19b68ff686
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-09-28 01:19:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,9 +8,9 @@ def load_library(emoticon_file)
   emoticon_lib = { :get_meaning => {},
                    :get_emoticon => {} }
 
-  emoticons.each do |key, value|
+  emoticons.each do |meaning, value|
     english = value[0]
-    japanese = value[1]
+    japanese = value [1]
     emoticon_lib[:get_meaning][japanese] = meaning
     emoticon_lib[:get_emoticon][english] = japanese
   end
